7c3be1b1cb2ce9564f7f7517d18d18437db3bd70
twoentartian <twoentartian@hotmail.com>
2023-03-28 01:36:28 +0200

    add multi-thread support.

--- bin/large_scale_DFL_deployment/analyze_result.py
+++ bin/large_scale_DFL_deployment/analyze_result.py
@@ -5,9 +5,12 @@ import json
 import os
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+import multiprocessing
 
 log_file_name = "./node_log.txt"
 glog_file_name = "./log/DFL.INFO"
+temp_file_path = "./temp/"
+skip_if_temp_file_exist = True
 
 maximum_time_second = 16000
 
@@ -48,69 +51,96 @@ def get_accuracy(msg: str) -> (bool, float):
     return True, float(accuracy_str)
 
 
+def process_single_node(single_node):
+    if skip_if_temp_file_exist:
+        if os.path.exists(temp_file_path + single_node["name"] + ".csv"):
+            return
+
+    print("checking node: " + single_node["name"])
+    working_dir = os.path.join(os.curdir, single_node["folder"])
+    log = open(os.path.join(working_dir, log_file_name), "r")
+    df = pandas.DataFrame(columns=["event_type", "elapsed", "time", "start_time", "node", "accuracy"])
+
+    first_log = True
+    day_counter = 0
+    last_hour = 0
+
+    # get start date time
+    glog_log = open(os.path.join(working_dir, glog_file_name), "r")
+    pattern_date_time = re.compile(r'\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}')
+    start_time_str = pattern_date_time.findall(glog_log.readline())
+    start_time = datetime.strptime(start_time_str[0], '%Y/%m/%d %H:%M:%S')
+
+    for single_line in log.readlines():
+        is_glog, level, time_raw = is_log(str(single_line))
+        if not is_glog:
+            continue
+
+        if first_log:
+            first_log = False
+            last_hour = time_raw.hour
+
+        # do day shift
+        if time_raw.hour < last_hour:
+            day_counter = day_counter + 1
+        last_hour = time_raw.hour
+
+        # the date of time_raw is 1900/1/1
+        start_time_without_date = datetime(time_raw.year, time_raw.month, time_raw.day, hour=start_time.hour, minute=start_time.minute, second=start_time.second)
+        time = time_raw - start_time_without_date + timedelta(days=day_counter)
+
+
+        has_accuracy, accuracy = get_accuracy(str(single_line))
+        if has_accuracy:
+            df.loc[len(df.index)] = ["report_accuracy", time.total_seconds(), time_raw.strftime("%H:%M:%S"), start_time, single_node["name"], accuracy]
+    df.to_csv(temp_file_path + single_node["name"] + ".csv")
+
+
 if __name__ == "__main__":
     # find summary.json
     with open("summary.json", "rb") as file:
         data = file.read()
         summary = json.loads(data)
 
-    df = pandas.DataFrame(columns=["event_type", "elapsed", "time", "start_time", "node", "accuracy"])
     # nodes
-    for single_node in summary["nodes"]:
-        print("checking node: " + single_node["name"])
-        working_dir = os.path.join(os.curdir, single_node["folder"])
-        log = open(os.path.join(working_dir, log_file_name), "r")
+    if not os.path.exists(temp_file_path):
+        os.mkdir(temp_file_path)
+    with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+        for single_node in summary["nodes"]:
+            pool.apply_async(process_single_node, (single_node, ))
+        pool.close()
+        pool.join()
 
-        first_log = True
-        day_counter = 0
-        last_hour = 0
-
-        # get start date time
-        glog_log = open(os.path.join(working_dir, glog_file_name), "r")
-        pattern_date_time = re.compile(r'\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}')
-        start_time_str = pattern_date_time.findall(glog_log.readline())
-        start_time = datetime.strptime(start_time_str[0], '%Y/%m/%d %H:%M:%S')
-
-        for single_line in log.readlines():
-            is_glog, level, time_raw = is_log(str(single_line))
-            if not is_glog:
-                continue
-
-            if first_log:
-                first_log = False
-                last_hour = time_raw.hour
-
-            # do day shift
-            if time_raw.hour < last_hour:
-                day_counter = day_counter + 1
-            last_hour = time_raw.hour
-
-            # the date of time_raw is 1900/1/1
-            start_time_without_date = datetime(time_raw.year, time_raw.month, time_raw.day, hour=start_time.hour, minute=start_time.minute, second=start_time.second)
-            time = time_raw - start_time_without_date + timedelta(days=day_counter)
-
-            has_accuracy, accuracy = get_accuracy(str(single_line))
-            if has_accuracy:
-                df.loc[len(df.index)] = ["report_accuracy", time.total_seconds(), time_raw.strftime("%H:%M:%S"), start_time, single_node["name"], accuracy]
+    # merge results
+    dfs = []
+    for single_entry in os.listdir(temp_file_path):
+        if os.path.isdir(single_entry):
+            continue
+        temp_df = pandas.read_csv(temp_file_path + single_entry, header=0, index_col=0)
+        dfs.append(temp_df)
+    df = pandas.concat(dfs)
+    print(df)
 
     # unify start time
-    first_start_time = df.min(axis='rows')["start_time"]
-
-
+    first_start_time_str = df.min(axis='rows')["start_time"]
+    print(first_start_time_str)
+    first_start_time = datetime.strptime(first_start_time_str, '%Y-%m-%d %H:%M:%S')
     def calculate_unify_time(row):
-        unified_elapsed_time = row["elapsed"] + (row['start_time'] - first_start_time).total_seconds()
+        start_time = datetime.strptime(row['start_time'], '%Y-%m-%d %H:%M:%S')
+        unified_elapsed_time = row["elapsed"] + (start_time - first_start_time).total_seconds()
         return unified_elapsed_time
-
 
     df["unified_elapsed_time"] = df.apply(func=calculate_unify_time, axis='columns')
     df.sort_values("unified_elapsed_time", axis=0, inplace=True)
-    print(df)
 
     # get line of each node
     df_for_each_node = {}
     node_names = df["node"].value_counts().index
     for node_name in node_names:
         df_for_each_node[node_name] = df.loc[(df.node == node_name) & (df.event_type == "report_accuracy")]
+
+    # save df
+    df.to_csv("./event_table.csv")
 
     fig = plt.figure()
     axs = fig.add_subplot(111)
@@ -122,8 +152,7 @@ if __name__ == "__main__":
     axs.set_xlabel('time (seconds)')
     axs.set_ylabel('accuracy (0-1)')
     axs.set_ylim([0, 1])
-    axs.set_xlim([0,maximum_time_second])
-
+    axs.set_xlim([0, maximum_time_second])
 
     fig.tight_layout()
     fig.savefig('accuracy.pdf')
