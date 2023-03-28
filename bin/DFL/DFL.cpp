c60831fccc8edbae2d913d1ad6e7c5ac90454da1
twoentartian <twoentartian@hotmail.com>
2023-03-28 02:04:53 +0200

    avoid infinite error signal handling.

--- bin/DFL/DFL.cpp
+++ bin/DFL/DFL.cpp
@@ -430,7 +430,7 @@ int main(int argc, char **argv)
             auto stacktrace = boost::stacktrace::stacktrace();
             std::cerr << "get signal: " << signum << " stacktrace:\n" <<stacktrace;
             global_container::get()->clear();
-            LOG(FATAL) << "get signal: " << signum << " stacktrace:\n" << stacktrace;
+            LOG(INFO) << "get signal: " << signum << " stacktrace:\n" << stacktrace;
             exit(signum);
         };
 
