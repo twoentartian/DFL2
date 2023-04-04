2b868a53c520b54295f9c1f12c2dedc6bca13df8
twoentartian <twoentartian@hotmail.com>
2023-04-04 13:58:32 +0200

    add auto support for legacy gpu without mem pool

--- bin/tool/analyze_models.cu
+++ bin/tool/analyze_models.cu
@@ -24,7 +24,15 @@ __global__ void calculate_square_of_value(T* output, T* input0, T* input1, uint3
 
 class cuda_stream_manager
 {
+private:
+    bool _device_support_async_mem_management;
+    
 public:
+    bool get_device_support_async_mem_management() const
+    {
+        return _device_support_async_mem_management;
+    }
+    
     explicit cuda_stream_manager(size_t stream_count)
     {
         _cuda_streams.resize(stream_count);
@@ -32,6 +40,11 @@ public:
         {
             checkCudaErrors(cudaStreamCreate(&stream));
         }
+        int attr = 0, device = 0;
+        cudaGetDevice(&device);
+        cudaDeviceGetAttribute(&attr, cudaDevAttrMemoryPoolsSupported, device);
+        if (attr == 1) _device_support_async_mem_management = true;
+        else _device_support_async_mem_management = false;
     }
     
     cudaStream_t get_cuda_stream()
@@ -69,56 +82,66 @@ void sync_all_cuda_stream()
 
 void allocate_and_copy_device_memory(float** temp_device_ptr, const float* host_data, size_t size)
 {
-#if ANALYZE_MODEL_USE_SINGLE_CUDA_STREAM
-    checkCudaErrors(cudaMalloc((void **)temp_device_ptr, size));
-    checkCudaErrors(cudaMemcpy(*temp_device_ptr, host_data, size, cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaStreamSynchronize(cudaStream_t(0)));
-#else
-    checkCudaErrors(cudaMallocAsync((void **)temp_device_ptr, size, cudaStream_t(0)));
-    checkCudaErrors(cudaMemcpyAsync(*temp_device_ptr, host_data, size, cudaMemcpyHostToDevice, cudaStream_t(0)));
-    checkCudaErrors(cudaStreamSynchronize(cudaStream_t(0)));
-#endif
+    if (static_cuda_stream_manager.get_device_support_async_mem_management())
+    {
+        checkCudaErrors(cudaMallocAsync((void **)temp_device_ptr, size, cudaStream_t(0)));
+        checkCudaErrors(cudaMemcpyAsync(*temp_device_ptr, host_data, size, cudaMemcpyHostToDevice, cudaStream_t(0)));
+        checkCudaErrors(cudaStreamSynchronize(cudaStream_t(0)));
+    }
+    else
+    {
+        checkCudaErrors(cudaMalloc((void **)temp_device_ptr, size));
+        checkCudaErrors(cudaMemcpy(*temp_device_ptr, host_data, size, cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaStreamSynchronize(cudaStream_t(0)));
+    }
 }
 
 std::vector<float> run_kernel(const std::vector<float>& weight_l, float* lhs_device_data, float* rhs_device_data)
 {
-#if not ANALYZE_MODEL_USE_SINGLE_CUDA_STREAM
     auto cuda_stream = static_cuda_stream_manager.get_cuda_stream();
-#endif
     
     //allocate output region on gpu
     auto output_size_bit = weight_l.size() * sizeof(weight_l[0]);
     float* output_dev;
-
-#if ANALYZE_MODEL_USE_SINGLE_CUDA_STREAM
-    checkCudaErrors(cudaMalloc((void **)&output_dev, output_size_bit));
-#else
-    checkCudaErrors(cudaMallocAsync((void **)&output_dev, output_size_bit, cuda_stream));
-#endif
-
+    
+    if (static_cuda_stream_manager.get_device_support_async_mem_management())
+    {
+        checkCudaErrors(cudaMallocAsync((void **)&output_dev, output_size_bit, cuda_stream));
+    }
+    else
+    {
+        checkCudaErrors(cudaMalloc((void **)&output_dev, output_size_bit));
+    }
     
     size_t data_size = weight_l.size();
     uint32_t block_count = data_size / BLOCK_SIZE + 1;
-#if ANALYZE_MODEL_USE_SINGLE_CUDA_STREAM
-    calculate_square_of_value<float><<<block_count, BLOCK_SIZE>>>(output_dev, lhs_device_data, rhs_device_data, weight_l.size());
-#else
-    calculate_square_of_value<float><<<block_count, BLOCK_SIZE, 0, cuda_stream>>>(output_dev, lhs_device_data, rhs_device_data, weight_l.size());
-#endif
+    if (static_cuda_stream_manager.get_device_support_async_mem_management())
+    {
+        calculate_square_of_value<float><<<block_count, BLOCK_SIZE, 0, cuda_stream>>>(output_dev, lhs_device_data, rhs_device_data, weight_l.size());
+    }
+    else
+    {
+        calculate_square_of_value<float><<<block_count, BLOCK_SIZE>>>(output_dev, lhs_device_data, rhs_device_data, weight_l.size());
+    }
 
     getLastCudaError("fail to start kernel <<<calculate_square_of_value>>>");
 
     //copy back to host
     std::vector<float> host_buffer;
     host_buffer.resize(weight_l.size());
-#if ANALYZE_MODEL_USE_SINGLE_CUDA_STREAM
-    checkCudaErrors(cudaMemcpy(host_buffer.data(), output_dev, output_size_bit, cudaMemcpyDeviceToHost));
-    checkCudaErrors(cudaFree(output_dev));
-    checkCudaErrors(cudaStreamSynchronize(cudaStream_t(0)));
-#else
-    checkCudaErrors(cudaMemcpyAsync(host_buffer.data(), output_dev, output_size_bit, cudaMemcpyDeviceToHost, cuda_stream));
-    checkCudaErrors(cudaFreeAsync(output_dev, cuda_stream));
-    checkCudaErrors(cudaStreamSynchronize(cuda_stream));
-#endif
+    
+    if (static_cuda_stream_manager.get_device_support_async_mem_management())
+    {
+        checkCudaErrors(cudaMemcpyAsync(host_buffer.data(), output_dev, output_size_bit, cudaMemcpyDeviceToHost, cuda_stream));
+        checkCudaErrors(cudaFreeAsync(output_dev, cuda_stream));
+        checkCudaErrors(cudaStreamSynchronize(cuda_stream));
+    }
+    else
+    {
+        checkCudaErrors(cudaMemcpy(host_buffer.data(), output_dev, output_size_bit, cudaMemcpyDeviceToHost));
+        checkCudaErrors(cudaFree(output_dev));
+        checkCudaErrors(cudaStreamSynchronize(cudaStream_t(0)));
+    }
     
     return host_buffer;
 }
@@ -127,12 +150,14 @@ void clear_gpu_memory(const std::map<std::string, float*>& node_layer_to_device_
 {
     for (const auto& [_, device_memory] : node_layer_to_device_memory)
     {
-#if ANALYZE_MODEL_USE_SINGLE_CUDA_STREAM
-        checkCudaErrors(cudaFree(device_memory));
-#else
-        checkCudaErrors(cudaFreeAsync(device_memory, cudaStream_t(0)));
-#endif
-
+        if (static_cuda_stream_manager.get_device_support_async_mem_management())
+        {
+            checkCudaErrors(cudaFreeAsync(device_memory, cudaStream_t(0)));
+        }
+        else
+        {
+            checkCudaErrors(cudaFree(device_memory));
+        }
     }
 }
 
