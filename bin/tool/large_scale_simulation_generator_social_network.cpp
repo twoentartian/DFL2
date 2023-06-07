48ee50039049b7a6406f961b2b9b2f73a976f793
twoentartian <twoentartian@hotmail.com>
2023-06-07 15:22:20 +0200

    performance improvement

--- bin/tool/large_scale_simulation_generator_social_network.cpp
+++ bin/tool/large_scale_simulation_generator_social_network.cpp
@@ -222,7 +222,7 @@ int main(int argc, char *argv[])
                 bool whole_success = false;
                 std::atomic_uint try_count = 0;
                 const size_t try_maximum = 1000;
-                std::atomic_bool stop = false;
+                bool stop = false;
                 std::mutex set_output_lock;
                 tmt::ParallelExecution_StepIncremental([&](int thread_index, int total_thread){
                     if (stop) return;
