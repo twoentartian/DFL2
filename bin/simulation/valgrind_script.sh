9dd1582c4ac0412acd31596c53a653ced1d9a6e4
twoentartian <twoentartian@hotmail.com>
2023-06-11 00:52:16 +0200

    add script record.

--- bin/simulation/valgrind_script.sh
+++ bin/simulation/valgrind_script.sh
@@ -1,2 +1,4 @@
 valgrind --tool=dhat --trace-children=yes --fair-sched=yes  ./DFL_simulator_opti
 
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --trace-children=yes --fair-sched=yes  --log-file=valgrind-200.txt  ./DFL_simulator_opti
+
