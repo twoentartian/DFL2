58a9e7c4a28bdefe1742fe5623a74dbf130aad30
twoentartian <twoentartian@hotmail.com>
2023-07-27 01:45:03 +0200

    add auto-find all sub-folder.

--- py_tool/draw_info.py
+++ py_tool/draw_info.py
@@ -1,4 +1,9 @@
+import math
+import os
+
 maximum_tick = 10000
+
+use_all_folders = True
 
 row = 3
 col = 4
@@ -15,3 +20,13 @@ titles = ["100_node",
           "400_node", "800_node", "1600_node", "2400_node", "3200_node", "4800_node", "6400_node", "8000_node"
 
           ]
+
+if use_all_folders:
+    exclusion_list = ['__pycache__', '.idea']
+    main_folder_path = '.'
+    items = os.listdir(main_folder_path)
+    folders = [item for item in items if os.path.isdir(os.path.join(main_folder_path, item)) and item not in exclusion_list]
+    print(folders)
+    titles = folders
+    row = math.ceil(math.sqrt(len(folders)))
+    col = int(len(folders) / row) + 1
