1f1ae3c8c34d614ee4e37b12849a396f1a704f6b
twoentartian <twoentartian@hotmail.com>
2023-08-20 01:18:55 +0200

    new scripts.

--- py_tool/nx_modify_network.py
+++ py_tool/nx_modify_network.py
@@ -1,4 +1,6 @@
 import argparse
+import random
+
 import networkx as nx
 import copy
 from pathlib import Path
@@ -11,7 +13,8 @@ if __name__ == "__main__":
     parser.add_argument("--rewire_ratio", nargs='+', help="the ratio of rewiring edges, can be an array: --rewire_ratio 0.01 0.02 0.04 0.08 0.16 0.32")
     parser.add_argument("--rewire_count", nargs='+', help="the count of rewiring edges, can be an array: --rewire_count 1 10 100 1000")
     parser.add_argument("--remove_hubs", nargs='+', help="number of largest hubs to remove, can be an array: --remove_hubs 1 2 4 8 16 32")
-    parser.add_argument("--remove_hubs_above", nargs='+', help="remove the hubs above the limit, can be an array: --keep_hubs_below 100 500 1000")
+    parser.add_argument("--remove_hubs_above", nargs='+', help="remove the hubs above the limit, can be an array: --remove_hubs_above 100 500 1000")
+    parser.add_argument("--downgrade_hubs", nargs="+", help="reduce hubs' degree to n: --downgrade_hubs n 100 500 1000")
 
     args = parser.parse_args()
     config = vars(args)
@@ -29,6 +32,8 @@ if __name__ == "__main__":
     if config["remove_hubs"]:
         count += 1
     if config["remove_hubs_above"]:
+        count += 1
+    if config["downgrade_hubs"]:
         count += 1
     if count > 1:
         print("please specify only 1 optional parameter per time")
@@ -59,13 +64,13 @@ if __name__ == "__main__":
 
     # remove_hubs
     if config["remove_hubs"]:
-        for remove_hub_count in config["remove_hubs"]:
+        for downgrade_hub_count in config["remove_hubs"]:
             G_temp = copy.deepcopy(G)
             node_degree_sorted = sorted(G_temp.degree, key=lambda x: x[1], reverse=True)
-            for i in range(0, int(remove_hub_count)):
+            for i in range(0, int(downgrade_hub_count)):
                 G_temp.remove_node(node_degree_sorted[i][0])
 
-            name = file_name + "." + "remove_hub" + str(remove_hub_count)
+            name = file_name + "." + "remove_hub" + str(downgrade_hub_count)
             nx_lib.generate_topology_file(G_temp, name)
             nx_lib.save_network_info(G_temp, name)
 
@@ -79,5 +84,37 @@ if __name__ == "__main__":
                     nodes_to_remove.append(node_degree[0])
             G_temp.remove_nodes_from(nodes_to_remove)
             name = file_name + "." + "remove_hubs_above" + remove_hubs_above
+            nx_lib.generate_topology_file(G_temp, name)
+            nx_lib.save_network_info(G_temp, name)
+
+    # downgrade_hubs
+    if config["downgrade_hubs"]:
+        downgrade_hubs_args = config["downgrade_hubs"]
+        downgrade_to_k = downgrade_hubs_args[0]
+        hub_count_list = downgrade_hubs_args[1:]
+        for downgrade_hub_count in hub_count_list:
+            G_temp = copy.deepcopy(G)
+            node_degree_sorted = sorted(G_temp.degree, key=lambda x: x[1], reverse=True)
+            for i in range(0, int(downgrade_hub_count)):
+                to_be_removed_node_name = node_degree_sorted[i][0]
+                neighbors = set(G.neighbors(to_be_removed_node_name))
+                G_temp.remove_node(node_degree_sorted[i][0])
+                if len(neighbors) % 2 == 0:
+                    # 4 nodes connect to removed_node
+                    reconnect_list = random.sample(list(neighbors), 4)
+                    for i in reconnect_list:
+                        G_temp.add_edge(to_be_removed_node_name, i)
+                        neighbors.remove(i)
+                else:
+                    reconnect_list = random.sample(list(neighbors), 3)
+                    for i in reconnect_list:
+                        G_temp.add_edge(to_be_removed_node_name, i)
+                        neighbors.remove(i)
+                assert len(neighbors) % 2 == 0
+                neighbors = list(neighbors)
+                random.shuffle(neighbors)
+                for i in range(0, len(neighbors), 2):
+                    G_temp.add_edge(neighbors[i], neighbors[i+1])
+            name = file_name + "." + "downgrade_hubs" + downgrade_hub_count
             nx_lib.generate_topology_file(G_temp, name)
             nx_lib.save_network_info(G_temp, name)
