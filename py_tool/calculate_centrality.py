9e8b2043ab707fc546c56652ea63264f44e50139
twoentartian <twoentartian@hotmail.com>
2023-08-22 01:03:23 +0200

    update script.

--- py_tool/calculate_centrality.py
+++ py_tool/calculate_centrality.py
@@ -1,15 +1,23 @@
 import os
 import json
 import networkx as nx
-import matplotlib.pyplot as plt
+import pandas as pd
 
 import draw_info
+
+
+def sum_of_deviations_from_max(values):
+    max_val = max(values)
+    return sum(max_val - v for v in values)
+
 
 if __name__ == "__main__":
     assert len(draw_info.folders) <= draw_info.row * draw_info.col
     folder_names_set = set()
+    output_df = pd.DataFrame(columns=['simulation_config', 'simulation_case', 'centrality'])
     for folder_index in range(len(draw_info.folders)):
         folder = draw_info.folders[folder_index]
+        print(f"processing {folder}")
         assert not (folder in folder_names_set)
         folder_names_set.add(folder)
         current_col = folder_index % draw_info.col
@@ -31,14 +39,21 @@ if __name__ == "__main__":
                 if len(dirLink) != 1:
                     G.add_edge(dirLink[0], dirLink[1])
 
-            eigenvector_centrality = nx.eigenvector_centrality(G)
-            norm_eigenvector_centrality = [len(G.nodes) * v for v in eigenvector_centrality.values()]
-            plt.figure(figsize=(10, 7))
-            nx.draw(G,
-                    with_labels=True,
-                    node_color=list(eigenvector_centrality.values()),
-                    node_size=norm_eigenvector_centrality,
-                    cmap=plt.cm.Reds,
-                    )
-            plt.show()
-            input()
+            node_centrality = nx.current_flow_betweenness_centrality(G)
+            graph_centrality = sum_of_deviations_from_max(list(node_centrality.values()))
+
+            # https://igraph.org/c/doc/igraph-Structural.html#igraph_centralization
+            network_size = len(G.nodes)
+            star_network = nx.star_graph(network_size-1)
+            star_network_centrality = nx.current_flow_betweenness_centrality(star_network)
+            star_graph_centrality = sum_of_deviations_from_max(list(star_network_centrality.values()))
+            graph_centrality = graph_centrality / star_graph_centrality
+            f = open(each_test_result_folder + "/centrality.txt", "w")
+            f.write(f"{graph_centrality}")
+            f.close()
+
+            output_df.loc[len(output_df)] = [folder, each_test_result_folder, graph_centrality]
+
+    output_df.to_csv("centrality.csv")
+
+
