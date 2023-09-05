8ade67f6083cdb7905f70f16fecdde629d4d3a95
twoentartian <twoentartian@hotmail.com>
2023-09-06 00:17:04 +0200

    minor change

--- py_tool/draw_herd_effect_delay_from_csv.py
+++ py_tool/draw_herd_effect_delay_from_csv.py
@@ -34,6 +34,33 @@ if __name__ == "__main__":
         fig.savefig(os.path.join(output_path, 'x_is_log.pdf'))
         fig.savefig(os.path.join(output_path, 'x_is_log.jpg'))
 
+        # x-axis and y-axis are log
+        fig, axs = plt.subplots(1, 1, figsize=(x_size, y_size))
+        axs.plot(herd_effect_delay_df["size"], herd_effect_delay_df['herd_effect_delay'])
+        axs.set_xlabel('network size')
+        axs.set_xscale('log')
+        axs.set_yscale('log')
+        axs.set_ylabel('herd effect delay (tick)')
+        axs.grid()
+        fig.savefig(os.path.join(output_path, 'x_y_is_log.pdf'))
+        fig.savefig(os.path.join(output_path, 'x_y_is_log.jpg'))
+
+        # combine raw and x,y log
+        fig, axs = plt.subplots(2, 1, figsize=(x_size, y_size))
+        axs[0].plot(herd_effect_delay_df["size"], herd_effect_delay_df['herd_effect_delay'])
+        axs[0].set_xlabel('network name')
+        axs[0].set_ylabel('herd effect delay (tick)')
+        axs[0].grid()
+        axs[1].plot(herd_effect_delay_df["size"], herd_effect_delay_df['herd_effect_delay'])
+        axs[1].set_xlabel('network name')
+        axs[1].set_ylabel('herd effect delay (tick)')
+        axs[1].set_xscale('log')
+        axs[1].set_yscale('log')
+        axs[1].grid()
+        fig.savefig(os.path.join(output_path, 'combine_raw_and_xy_log.pdf'))
+        fig.savefig(os.path.join(output_path, 'combine_raw_and_xy_log.jpg'))
+
+
         # x axis is log, y axis is smoothed (15)
         fig, axs = plt.subplots(1, 1, figsize=(x_size, y_size))
         smooth_window_size = 15
