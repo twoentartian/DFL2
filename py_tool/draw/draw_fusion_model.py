f0416698f1c96ff8bac6dae9a3fd1e678f2eebf9
twoentartian <twoentartian@hotmail.com>
2024-04-02 17:37:10 +0200

    add args to draw loss/accuracy

--- py_tool/draw/draw_fusion_model.py
+++ py_tool/draw/draw_fusion_model.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import argparse
+import os
 
 fusion_accuracy_mapping_file_path = "fusion_model_accuracy.csv"
 
@@ -11,10 +13,10 @@ def get_propagating_src(fusion_accuracy_mapping):
     return propagating_src
 
 
-def draw_fusion_accuracy_loss_graph(fusion_df, save_name):
+def draw_fusion_accuracy_loss_graph(fusion_df, command, save_name):
     propagating_src = get_propagating_src(fusion_df)
 
-    value_to_plot = "accuracy"
+    value_to_plot = command
     if len(propagating_src) == 3:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -28,7 +30,7 @@ def draw_fusion_accuracy_loss_graph(fusion_df, save_name):
     elif len(propagating_src) == 2:
         plt.figure()
         sc = plt.scatter(fusion_df[propagating_src[0]], fusion_df[propagating_src[1]], c=fusion_df[value_to_plot], s=5, cmap='viridis')
-        plt.colorbar(sc, label='Accuracy')
+        plt.colorbar(sc, label=command)
         plt.title(f'Heatmap of {value_to_plot.capitalize()}')
         plt.xlabel('Dimension 1')
         plt.ylabel('Dimension 0')
@@ -46,6 +48,32 @@ def draw_fusion_accuracy_loss_graph(fusion_df, save_name):
         exit(-1)
 
 
+def get_file_name_without_extension(file_path):
+    # Split the file path to get the file name and extension
+    file_name_with_extension = os.path.basename(file_path)
+
+    # Split the file name and extension
+    file_name, file_extension = os.path.splitext(file_name_with_extension)
+
+    return file_name
+
+
 if __name__ == "__main__":
-    fusion_df = pd.read_csv(fusion_accuracy_mapping_file_path)
-    draw_fusion_accuracy_loss_graph(fusion_df, "fusion_model_accuracy.pdf")
+    parser = argparse.ArgumentParser(description='Process loss or accuracy data.')
+
+    # Add the command argument
+    parser.add_argument('command', choices=['loss', 'accuracy'], help='Specify the command (loss or accuracy)')
+
+    # Add the file path argument
+    parser.add_argument('file_path', nargs='?', type=str, default=fusion_accuracy_mapping_file_path, help='File path to process')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Retrieve the values
+    command = args.command
+    file_path = args.file_path
+
+    fusion_df = pd.read_csv(file_path)
+    file_name = get_file_name_without_extension(file_path)
+    draw_fusion_accuracy_loss_graph(fusion_df, command, f"{file_name}__{command}.pdf")
