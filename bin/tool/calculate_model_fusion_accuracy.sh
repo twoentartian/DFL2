e2c92953baf0a594f39bc0ea56a1d63492ce71aa
twoentartian <twoentartian@hotmail.com>
2024-04-08 22:38:21 +0200

    typo fix

--- bin/tool/calculate_model_fusion_accuracy.sh
+++ bin/tool/calculate_model_fusion_accuracy.sh
@@ -3,5 +3,5 @@ export OPENBLAS_NUM_THREADS=1
 export GOTO_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-chomd +x ./calculate_model_fusion_accuracy
+chmod +x ./calculate_model_fusion_accuracy
 ./calculate_model_fusion_accuracy "$@"
